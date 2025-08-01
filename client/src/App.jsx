a03a2b9b363e5d678c9a8fe9de906818fbb302c6
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-08-01 22:45:49 +1200

    chore: imported Search function to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import styles from './App.module.css'
 
+// import SecondNav from './components/shared/SecondNav'
+// import Navbar from './components/shared/Navbar'
+import Search from './components/features/search/Search'
+
 function App() {
 
   return (
     <>
+    {/* <Navbar/> */}
+    {/* <SecondNav/> */}
+    <Search/>
     </>
   )
 }
