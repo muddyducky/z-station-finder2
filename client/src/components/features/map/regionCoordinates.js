78c2983c1960a2000730ec7cd307a1070cd7d91b
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-08-10 13:50:09 +1200

    feat: regional coordinates for cluster markers

---
+++ client/src/components/features/map/regionCoordinates.js
@@ -0,0 +1,13 @@
+//__ Map region coordinates
+
+export const regionCoordinates = {
+  Auckland: { lat: -36.8485, lng: 174.7633 },
+  Waikato: { lat: -37.7833, lng: 175.2833 },
+  Wellington: { lat: -41.2865, lng: 174.7762 },
+  Nelson: { lat: -41.2706, lng: 173.2839 },
+  Marlborough: { lat: -41.5111, lng: 173.9510 },
+  'West Coast': { lat: -42.4500, lng: 171.2000 },
+  "Bay of Plenty": { lat: -37.6878, lng: 176.1651 },
+  Canterbury: { lat: -43.5321, lng: 172.6362 },
+  Southland: { lat: -46.4132, lng: 168.3538 }
+};
