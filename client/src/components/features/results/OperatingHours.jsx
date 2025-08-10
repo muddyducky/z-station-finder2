e942a9ada5565bf7cae8b22a8954c7f17d3eac8f
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-08-10 13:53:03 +1200

    update: added comments

--- client/src/components/features/results/OperatingHours.jsx
+++ client/src/components/features/results/OperatingHours.jsx
@@ -1,9 +1,16 @@
 import styles from "./OperatingHours.module.css";
+
+//___ GetFormattedHours utility that formats the operating hours
 import GetFormattedHours from "../../../utils/FormattedHours";
 import { useState } from "react";
 
+//__ OperatingHours component that displays the operating hours of a station
 export default function OperatingHours({ hours }) {
+
+  //__ State to manage the dropdown visibility
   const [isOpen, setIsOpen] = useState(true);
+
+  //___ Array for days of the week to use in the dropdown
   const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   const formattedHours = GetFormattedHours(hours);
 
@@ -11,10 +18,10 @@ export default function OperatingHours({ hours }) {
     return null;
   }
 
+  //___ Function to toggle the dropdown visibility
   const toggleDropdownClick = () => {
     setIsOpen((prev) => !prev);
   };
-  console.log("GetFormattedHours output:", formattedHours);
 
   return (
     <div className={styles.operatingHoursContainer}>
@@ -22,11 +29,15 @@ export default function OperatingHours({ hours }) {
         Open Hours{" "}
         <img
           src="/icons/misc/Dropdown.png"
-          alt=""
+          alt="dropdown-icon"
+
+          //___ Add a click listener to the dropdown icon
           onClick={toggleDropdownClick}
           className={`${styles.dropdown} ${isOpen ? styles.open : ""}`}
         />
       </p>
+
+      {/* Dropdown list for operating hours */}
       {isOpen && (
         <div className={`${styles.hoursList} ${!isOpen ? styles.closed : "" }`}>
           {daysOfWeek.map((day, index) => (
