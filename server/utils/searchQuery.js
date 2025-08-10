20eb1515ce3646ec69d5d339c376d28210b621c6
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-08-10 14:02:47 +1200

    fix: improve searchQuery to support exact region matches for stricter filtering

--- server/utils/searchQuery.js
+++ server/utils/searchQuery.js
@@ -1,26 +1,39 @@
-const searchQuery = (keyword) => {
-    if(!keyword) return {};
-        const searchTerms = keyword.trim().split(/\s+/).filter(term => term.length > 0)
+const searchQuery = (keyword, strictRegionMatch) => {
+  if (!keyword) return {};
+  const searchTerms = keyword
+    .trim()
+    .split(/\s+/)
+    .filter((term) => term.length > 0);
 
-        const locationFields = ["location.city", "location.region", "location.address"]
-        const serviceFields = ["services.name"]
-        const fuelFields = ["fuel.type"]
+  const locationFields = [
+    "location.city",
+    "location.region",
+    "location.address",
+  ];
+  
+  const serviceFields = ["services.name"];
+  const fuelFields = ["fuel.type"];
+  const queryConditions = [];
 
-        const queryConditions = []
+  for (const term of searchTerms) {
+    const termRegex = new RegExp(term, "i");
+    const exactRegex = new RegExp(`^${term}$`, "i");
 
-        for(const term of searchTerms) {
-            const termRegex = new RegExp(term, "i")
-
-            queryConditions.push({
-                $or: [
-                ...locationFields.map(field => ({ [field]: termRegex})),
-                ...serviceFields.map(field => ({ [field]: termRegex})),
-                ...fuelFields.map(field => ({ [field]: termRegex})),
-
-            ]
-        })
-    }     
-    return {$and: queryConditions}
-}
+    queryConditions.push({
+      $or: [
+        ...locationFields.map((field) => {
+          const regex =
+            strictRegionMatch && field === "location.region"
+              ? exactRegex
+              : termRegex;
+          return { [field]: regex };
+        }),
+        ...serviceFields.map((field) => ({ [field]: termRegex })),
+        ...fuelFields.map((field) => ({ [field]: termRegex })),
+      ],
+    });
+  }
+  return { $and: queryConditions };
+};
 
 export default searchQuery;
