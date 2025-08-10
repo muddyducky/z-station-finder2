40f150e3d27a000a62d3421f345793a38c8f6756
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-08-10 13:53:42 +1200

    update: added comments

--- client/src/components/features/results/ResultModal1.jsx
+++ client/src/components/features/results/ResultModal1.jsx
@@ -1,6 +1,9 @@
 import styles from './ResultModal1.module.css'
+
+//__ StationCard component that displays a single station
 import StationCard from './StationCard'
 
+//__ ResultModal component that displays a list of stations
 export default function ResultModal({ stations }) {
   if(stations.length === 0) {
     return <p>No stations available...</p>
@@ -13,8 +16,13 @@ export default function ResultModal({ stations }) {
           <img src="/icons/misc/Close.png" alt="closeIcon" className={styles.closeIcon}/>
         </div>
         <p>showing {stations.length} results </p>
+
+      {/* Render a StationCard for each station */}
       {stations.map((station) => (
-        <StationCard key={station.id} station={station} />
+        <StationCard 
+          key={station.id} 
+          station={station} 
+        />
       ))}
     </div>
   )
