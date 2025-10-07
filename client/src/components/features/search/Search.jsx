3128607cefdfc3e1a8573f895fc02a06eb674c94
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-10-07 20:29:09 +1300

    feat: added toggle to search by price button

--- client/src/components/features/search/Search.jsx
+++ client/src/components/features/search/Search.jsx
@@ -1,8 +1,12 @@
-import styles from './Search.module.css'
-import SearchInput from './SearchInput.jsx'
+import styles from "./Search.module.css";
+import SearchByPrice from "./searchByPrice/SearchByPrice.jsx";
+import SearchInput from "./SearchInput.jsx";
+import { useState } from "react";
 
 //___ Search component that renders the search input and its functionality
-export default function Search({ onFilter, placeholder, className, keyword}) {
+export default function Search({ onFilter, placeholder, className, keyword, onShowPriceClick }) {
+  const [togglePrice, setTogglePrice] = useState(false);
+
   return (
     <div className={`${styles.searchPage} ${className}`}>
       <h1>Search Z Stations</h1>
@@ -14,6 +18,21 @@ export default function Search({ onFilter, placeholder, className, keyword}) {
           keyword={keyword}
         />
       </div>
+      <span>
+        <img src="/icons/misc/Geolocation.png" alt="location-icon" />
+        <p>Use my current location</p>
+      </span>
+
+      {/* ___ Search by price button and functionality */}
+      <div className={styles.centerPriceBtn}>
+        <SearchByPrice
+          className={styles.searchByPrice}
+          onFilter={onFilter}
+          togglePrice={togglePrice}
+          setTogglePrice={setTogglePrice}
+          onShowPriceClick={onShowPriceClick}
+        />
+      </div>
     </div>
-  )
+  );
 }
